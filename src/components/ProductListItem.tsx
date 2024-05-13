75c7779e8f4a8fed950edfb6567f99f013a2bbd3
Jair1810 <jair18101@hotmail.com>
2024-05-13 12:45:37 -0600

    expo version upgraded
    basic expo router implementation.
    
    change product.tsx to [id].tsx for dynamic routing

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,29 +1,33 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import Colors from "@/src/constants/Colors";
 import { Product } from "@/src/types";
+import { Link } from "expo-router";
 
 const defaultPizzaImage =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
-
+//Prop typing
 type ProductListItemProps = {
   product: Product;
 };
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.image}
-        //If image exist use product.image if not, defaultPizzaImage
-        source={{
-          uri: product.image || defaultPizzaImage,
-        }}
-        resizeMode="contain"
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+    //Navigate specific product
+    <Link href={`/${product.id}`} asChild>
+      <TouchableOpacity style={styles.container}>
+        <Image
+          style={styles.image}
+          //If image exist use product.image if not, defaultPizzaImage
+          source={{
+            uri: product.image || defaultPizzaImage,
+          }}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </TouchableOpacity>
+    </Link>
   );
 };
 
