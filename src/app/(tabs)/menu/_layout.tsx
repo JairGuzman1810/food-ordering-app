af59a0402d951efe1ca28037e9644d0ff251cd44
Jair1810 <jair18101@hotmail.com>
2024-05-13 17:47:41 -0600

    add shopping cart icon to the menu bar
    init cart modal
    
    changing modal configuration for cart
    
    remove modal.tsx

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,9 +1,34 @@
-import { Stack } from "expo-router";
+import Colors from "@/src/constants/Colors";
+import { FontAwesome } from "@expo/vector-icons";
+import { Link, Stack } from "expo-router";
+import { Pressable, TouchableOpacity } from "react-native";
 //Nested tab navigation of menu and product details
 export default function MenuStack() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Menu" }} />
+    <Stack
+      screenOptions={{
+        headerRight: () => (
+          <Link href="/cart" asChild>
+            <TouchableOpacity
+              style={{ marginRight: 15 }}
+              activeOpacity={0.5} // Set activeOpacity to achieve the opacity change on press
+            >
+              <FontAwesome
+                name="shopping-cart"
+                size={20}
+                color={Colors.light.tint}
+              />
+            </TouchableOpacity>
+          </Link>
+        ),
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Menu",
+        }}
+      />
     </Stack>
   );
 }
