20f870fe4e4abe0650ec2157314f05a05c6b5f9e
Jair1810 <jair18101@hotmail.com>
2024-05-13 14:35:43 -0600

    Added information to product details screen: -name -image -sizes (wip) -price

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,17 +1,74 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
+import products from "@/assets/data/products";
+import { defaultPizzaImage } from "@/src/components/ProductListItem";
+import Colors from "@/src/constants/Colors";
+
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
+  //Obtain product of dummy data by id
+  const product = products.find((p) => p.id.toString() === id);
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
   return (
-    <View>
-      <Stack.Screen options={{ title: "Details: " + id }} />
-      <Text>ProductDetailsScreen for id: {id}</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image
+        style={styles.image}
+        source={{ uri: product.image || defaultPizzaImage }}
+      />
+      <Text>Select size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <View key={size} style={styles.size}>
+            <Text style={styles.sizeText}>{size}</Text>
+          </View>
+        ))}
+      </View>
+
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 };
 
 export default ProductDetailsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.light.background,
+    padding: 10,
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  sizes: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "600",
+  },
+});
