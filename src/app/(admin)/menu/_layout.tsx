cddb95dd608294e89cc9a07fd9a617202b70b64c
Jair1810 <jair18101@hotmail.com>
2024-05-15 15:21:31 -0600

    expo upgraded expo-router upgraded
    changed dark mode and light mode colors from tabIcon
    
    added initial admin side
    
    temp navigation to admin and user

---
+++ src/app/(admin)/menu/_layout.tsx
@@ -0,0 +1,38 @@
+import Colors from "@/src/constants/Colors";
+import { FontAwesome } from "@expo/vector-icons";
+import { Link, Stack } from "expo-router";
+import { Pressable, TouchableOpacity } from "react-native";
+import { useColorScheme } from "@/src/components/useColorScheme";
+
+//Nested tab navigation of menu and product details
+export default function MenuStack() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerRight: () => (
+          <Link href="/cart" asChild>
+            <TouchableOpacity
+              style={{ marginRight: 15 }}
+              activeOpacity={0.5} // Set activeOpacity to achieve the opacity change on press
+            >
+              <FontAwesome
+                name="shopping-cart"
+                size={20}
+                color={Colors[colorScheme ?? "light"].tint}
+              />
+            </TouchableOpacity>
+          </Link>
+        ),
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Menu",
+        }}
+      />
+    </Stack>
+  );
+}
