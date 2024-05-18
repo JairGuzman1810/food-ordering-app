19b4f74802cb37225c320f41cb6fd48af21e8882
Jair1810 <jair18101@hotmail.com>
2024-05-17 19:37:42 -0600

    added dayjs for time
    order list for user ui
    
    removed two.tsx
    
    added orders types

---
+++ src/app/(user)/orders/[id].tsx
@@ -0,0 +1,124 @@
+import { Image, StyleSheet, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import products from "@/assets/data/products";
+import { defaultPizzaImage } from "@/src/components/ProductListItem";
+import Colors from "@/src/constants/Colors";
+import Button from "@/src/components/Button";
+import { Text } from "@/src/components/Themed";
+import { useColorScheme } from "@/src/components/useColorScheme";
+import { useCart } from "@providers/CartProvider";
+import { PizzaSize } from "@/src/types";
+
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
+
+const ProductDetailsScreen = () => {
+  const { id } = useLocalSearchParams();
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { addItem } = useCart();
+  const router = useRouter();
+  //Obtain product of dummy data by id
+  const product = products.find((p) => p.id.toString() === id);
+
+  const colorScheme = useColorScheme();
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+    setIsLoading(true);
+    // Perform your loading action here
+    // After the action is complete, set isLoading back to false
+    addItem(product, selectedSize);
+    setTimeout(() => {
+      setIsLoading(false);
+      router.push("/cart");
+    }, 500); // Example: Simulating loading for 2 seconds
+  };
+
+  return (
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: Colors[colorScheme ?? "light"].backgroundCard },
+      ]}
+    >
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image
+        style={styles.image}
+        source={{ uri: product.image || defaultPizzaImage }}
+      />
+      <Text>Select size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <TouchableOpacity
+            onPress={() => setSelectedSize(size)}
+            key={size}
+            disabled={isLoading}
+            style={[
+              styles.size,
+              {
+                backgroundColor:
+                  selectedSize === size ? "gainsboro" : undefined,
+              },
+            ]}
+          >
+            <Text
+              style={[
+                styles.sizeText,
+                {
+                  color: selectedSize === size ? "black" : "gray",
+                },
+              ]}
+            >
+              {size}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <Text style={styles.price}>${product.price}</Text>
+      <Button text="Add to cart" isLoading={isLoading} onPress={addToCart} />
+    </View>
+  );
+};
+
+export default ProductDetailsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  sizes: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginTop: "auto",
+  },
+});
---
+++ src/components/OrderListItem.tsx
@@ -0,0 +1,78 @@
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import React from "react";
+import Colors from "@/src/constants/Colors";
+import { Order } from "@/src/types";
+import { Link, useSegments } from "expo-router";
+import { Text } from "./Themed";
+import { useColorScheme } from "@/src/components/useColorScheme";
+import relativeTime from "dayjs/plugin/relativeTime";
+import dayjs from "dayjs";
+
+dayjs.extend(relativeTime);
+
+// Prop typing
+type OrderListItemProps = {
+  order: Order;
+};
+
+const OrderListItem = ({ order }: OrderListItemProps) => {
+  const segments = useSegments();
+  const colorScheme = useColorScheme();
+  const colors = colorScheme === "dark" ? Colors.dark : Colors.light;
+
+  return (
+    <Link
+      href={`/${segments[0]}/orders/${order.id}`}
+      style={styles.link}
+      asChild
+    >
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={[styles.touchable, { backgroundColor: colors.backgroundCard }]}
+        >
+          <View style={styles.infoContainer}>
+            <Text style={styles.orderId}>Order #{order.id}</Text>
+            <Text style={styles.createdAt}>
+              {dayjs(order.created_at).fromNow()}
+            </Text>
+          </View>
+          <Text style={styles.status}>{order.status}</Text>
+        </TouchableOpacity>
+      </View>
+    </Link>
+  );
+};
+
+export default OrderListItem;
+
+const styles = StyleSheet.create({
+  link: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+  },
+  touchable: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 10,
+    borderRadius: 10,
+
+    width: "100%",
+  },
+  infoContainer: {
+    flexDirection: "column",
+  },
+  orderId: {
+    fontWeight: "bold",
+    marginVertical: 5,
+  },
+  createdAt: {
+    color: "gray",
+  },
+  status: {
+    fontWeight: "500",
+    textAlign: "right",
+  },
+});
