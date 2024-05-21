a103ad1072dfcebc39dbfa1aba0cf6190befa904
Jair1810 <jair18101@hotmail.com>
2024-05-21 14:43:49 -0600

    added queryprovider

---
+++ src/providers/QueryProvider.tsx
@@ -0,0 +1,8 @@
+import React, { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
+
+export default function QueryProvider({ children }: PropsWithChildren) {
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
