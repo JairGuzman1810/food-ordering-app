7f56ca22557aca891f538b7974a3904f5aafd94f
Jair1810 <jair18101@hotmail.com>
2024-05-21 17:34:24 -0600

    get product by id added in api
    now the user and admin can get the specific product in [id]

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -1,7 +1,12 @@
-import { Image, StyleSheet, View, TouchableOpacity } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 import React, { useState } from "react";
 import { Link, Stack, useLocalSearchParams, useRouter } from "expo-router";
-import products from "@/assets/data/products";
 import { defaultPizzaImage } from "@/src/components/ProductListItem";
 import Colors from "@/src/constants/Colors";
 import Button from "@/src/components/Button";
@@ -9,54 +14,103 @@ import { Text } from "@/src/components/Themed";
 import { useColorScheme } from "@/src/components/useColorScheme";
 import { useCart } from "@providers/CartProvider";
 import { PizzaSize } from "@/src/types";
+import { useProduct } from "@/src/api/products";
 import { FontAwesome } from "@expo/vector-icons";
+
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
 
+  const productId = Array.isArray(id) ? id[0] : id || ""; // Take the first element if it's an array, or use an empty string if it's undefined
+
+  const { data: product, isLoading, error } = useProduct(parseInt(productId)); // Perform null check and convert ID to string if it exists
+
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
+  const [loading, setLoading] = useState(false);
+
+  const { addItem } = useCart();
   const router = useRouter();
   //Obtain product of dummy data by id
-  const product = products.find((p) => p.id.toString() === id);
 
   const colorScheme = useColorScheme();
 
-  if (!product) {
-    return <Text>Product not found</Text>;
-  }
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+    setLoading(true);
+    // Perform your loading action here
+    // After the action is complete, set isLoading back to false
+    addItem(product, selectedSize);
+    setTimeout(() => {
+      setLoading(false);
+      router.push("/cart");
+    }, 500); // Example: Simulating loading for 2 seconds
+  };
+
+  const LoadingIndicator = () => (
+    <View style={styles.centered}>
+      <ActivityIndicator size={"large"} />
+    </View>
+  );
+
+  const ErrorMessage = ({ message }: { message: string }) => (
+    <View style={styles.centered}>
+      <Text>Error: {message}</Text>
+    </View>
+  );
 
   return (
     <View
       style={[
         styles.container,
-        { backgroundColor: Colors[colorScheme ?? "light"].backgroundCard },
+        {
+          backgroundColor: Colors[colorScheme ?? "light"].backgroundCard,
+        },
       ]}
     >
-      <Stack.Screen
-        options={{
-          title: "Menu",
-          headerRight: () => (
-            <Link href={`/(admin)/menu/create?id=${id}`} asChild>
-              <TouchableOpacity
-                style={{ marginRight: 15 }}
-                activeOpacity={0.5} // Set activeOpacity to achieve the opacity change on press
-              >
-                <FontAwesome
-                  name="pencil"
-                  size={20}
-                  color={Colors[colorScheme ?? "light"].tint}
-                />
-              </TouchableOpacity>
-            </Link>
-          ),
-        }}
-      />
-      <Stack.Screen options={{ title: product?.name }} />
-      <Image
-        style={styles.image}
-        source={{ uri: product.image || defaultPizzaImage }}
-      />
+      {/* if still loading only show the loading icon */}
+      {isLoading && <LoadingIndicator />}
+      {/* if something goes wrong then show only message and not content */}
+      {error && <ErrorMessage message={error.message} />}
+      {/* if for some reason the product its not found, then show this */}
+      {!product && !isLoading && !error && (
+        <View style={styles.centered}>
+          <Text>Product not found</Text>
+        </View>
+      )}
+      {/* if product its load and there are not error show ui with data */}
+      {!isLoading && !error && product && (
+        <>
+          <Stack.Screen
+            options={{
+              title: "Menu",
+              headerRight: () => (
+                <Link href={`/(admin)/menu/create?id=${id}`} asChild>
+                  <TouchableOpacity
+                    style={{ marginRight: 15 }}
+                    activeOpacity={0.5} // Set activeOpacity to achieve the opacity change on press
+                  >
+                    <FontAwesome
+                      name="pencil"
+                      size={20}
+                      color={Colors[colorScheme ?? "light"].tint}
+                    />
+                  </TouchableOpacity>
+                </Link>
+              ),
+            }}
+          />
+          <Stack.Screen options={{ title: product?.name }} />
+          <Image
+            style={styles.image}
+            source={{ uri: product.image || defaultPizzaImage }}
+          />
 
-      <Text style={styles.price}>${product.price}</Text>
+          <Text style={styles.price}>${product.price}</Text>
+        </>
+      )}
     </View>
   );
 };
@@ -65,15 +119,37 @@ export default ProductDetailsScreen;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
     flex: 1,
+    padding: 10,
   },
   image: {
     width: "100%",
     aspectRatio: 1,
   },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
   price: {
     fontSize: 18,
     fontWeight: "600",
+    marginTop: "auto",
+  },
+  centered: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- src/app/(user)/menu/[id].tsx
+++ src/app/(user)/menu/[id].tsx
@@ -1,7 +1,12 @@
-import { Image, StyleSheet, View, TouchableOpacity } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 import React, { useState } from "react";
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
-import products from "@/assets/data/products";
 import { defaultPizzaImage } from "@/src/components/ProductListItem";
 import Colors from "@/src/constants/Colors";
 import Button from "@/src/components/Button";
@@ -9,81 +14,111 @@ import { Text } from "@/src/components/Themed";
 import { useColorScheme } from "@/src/components/useColorScheme";
 import { useCart } from "@providers/CartProvider";
 import { PizzaSize } from "@/src/types";
+import { useProduct } from "@/src/api/products";
 
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
+
+  const productId = Array.isArray(id) ? id[0] : id || ""; // Take the first element if it's an array, or use an empty string if it's undefined
+
+  const { data: product, isLoading, error } = useProduct(parseInt(productId)); // Perform null check and convert ID to string if it exists
+
   const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
-  const [isLoading, setIsLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const { addItem } = useCart();
   const router = useRouter();
   //Obtain product of dummy data by id
-  const product = products.find((p) => p.id.toString() === id);
 
   const colorScheme = useColorScheme();
-
-  if (!product) {
-    return <Text>Product not found</Text>;
-  }
 
   const addToCart = () => {
     if (!product) {
       return;
     }
-    setIsLoading(true);
+    setLoading(true);
     // Perform your loading action here
     // After the action is complete, set isLoading back to false
     addItem(product, selectedSize);
     setTimeout(() => {
-      setIsLoading(false);
+      setLoading(false);
       router.push("/cart");
     }, 500); // Example: Simulating loading for 2 seconds
   };
+
+  const LoadingIndicator = () => (
+    <View style={styles.centered}>
+      <ActivityIndicator size={"large"} />
+    </View>
+  );
+
+  const ErrorMessage = ({ message }: { message: string }) => (
+    <View style={styles.centered}>
+      <Text>Error: {message}</Text>
+    </View>
+  );
 
   return (
     <View
       style={[
         styles.container,
-        { backgroundColor: Colors[colorScheme ?? "light"].backgroundCard },
+        {
+          backgroundColor: Colors[colorScheme ?? "light"].backgroundCard,
+        },
       ]}
     >
-      <Stack.Screen options={{ title: product?.name }} />
-      <Image
-        style={styles.image}
-        source={{ uri: product.image || defaultPizzaImage }}
-      />
-      <Text>Select size</Text>
-      <View style={styles.sizes}>
-        {sizes.map((size) => (
-          <TouchableOpacity
-            onPress={() => setSelectedSize(size)}
-            key={size}
-            disabled={isLoading}
-            style={[
-              styles.size,
-              {
-                backgroundColor:
-                  selectedSize === size ? "gainsboro" : undefined,
-              },
-            ]}
-          >
-            <Text
-              style={[
-                styles.sizeText,
-                {
-                  color: selectedSize === size ? "black" : "gray",
-                },
-              ]}
-            >
-              {size}
-            </Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-      <Text style={styles.price}>${product.price}</Text>
-      <Button text="Add to cart" isLoading={isLoading} onPress={addToCart} />
+      {/* if still loading only show the loading icon */}
+      {isLoading && <LoadingIndicator />}
+      {/* if something goes wrong then show only message and not content */}
+      {error && <ErrorMessage message={error.message} />}
+      {/* if for some reason the product its not found, then show this */}
+      {!product && !isLoading && !error && (
+        <View style={styles.centered}>
+          <Text>Product not found</Text>
+        </View>
+      )}
+      {/* if product its load and there are not error show ui with data */}
+      {!isLoading && !error && product && (
+        <>
+          <Stack.Screen options={{ title: product.name }} />
+          <Image
+            style={styles.image}
+            source={{ uri: product.image || defaultPizzaImage }}
+          />
+          <Text>Select size</Text>
+          <View style={styles.sizes}>
+            {sizes.map((size) => (
+              <TouchableOpacity
+                onPress={() => setSelectedSize(size)}
+                key={size}
+                disabled={loading}
+                style={[
+                  styles.size,
+                  {
+                    backgroundColor:
+                      selectedSize === size ? "gainsboro" : undefined,
+                  },
+                ]}
+              >
+                <Text
+                  style={[
+                    styles.sizeText,
+                    {
+                      color: selectedSize === size ? "black" : "gray",
+                    },
+                  ]}
+                >
+                  {size}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+          <Text style={styles.price}>${product.price}</Text>
+          <Button text="Add to cart" isLoading={loading} onPress={addToCart} />
+        </>
+      )}
     </View>
   );
 };
@@ -92,15 +127,14 @@ export default ProductDetailsScreen;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
     flex: 1,
+    padding: 10,
   },
   image: {
     width: "100%",
     aspectRatio: 1,
   },
   sizes: {
-    display: "flex",
     flexDirection: "row",
     justifyContent: "space-around",
     marginVertical: 10,
@@ -120,5 +154,10 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "600",
     marginTop: "auto",
+  },
+  centered: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
