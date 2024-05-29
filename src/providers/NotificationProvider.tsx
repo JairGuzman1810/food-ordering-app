f952368e9c4162a252fe77861424991aec273815
Jair1810 <jair18101@hotmail.com>
2024-05-28 18:29:23 -0600

    notification push

---
+++ src/providers/NotificationProvider.tsx
@@ -0,0 +1,51 @@
+import { View, Text } from "react-native";
+import React, { PropsWithChildren, useEffect, useRef, useState } from "react";
+import { registerForPushNotificationsAsync } from "../lib/notifications";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+const NotificationProvider = ({ children }: PropsWithChildren) => {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined);
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ""))
+      .catch((error: any) => setExpoPushToken(`${error}`));
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  console.log(notification);
+
+  return <>{children}</>;
+};
+
+export default NotificationProvider;
